a3e49272319c384e90ece0c0b68af4138f266703
Meganathan <meganathan212003@gmail.com>
2023-12-28 21:12:26 +0530

    remove spaced

--- JS LEARNING BASIC/Factory_funct.js
+++ JS LEARNING BASIC/Factory_funct.js
@@ -1,20 +1,20 @@
 /* Factory functioins  It's nothing but if we create a new object it will return the inside the objects */
 
 //Example  we can create a dynamic greeting
-// function createPerson(name){
-//     return{
-//         name,
-//         greeting(){
-//             let msg = "Hi my name is " + this.name ;
-//             console.log(msg);
-//         }
-//     };
-// }
-// let name = createPerson("dinesh");
-// console.log(name);
-// function greeting(){
+function createPerson(name){
+    return{
+        name,
+        greeting(){
+            let msg = "Hi my name is " + this.name ;
+            console.log(msg);
+        }
+    };
+}
+let name = createPerson("dinesh");
+console.log(name);
+function greeting(){
 
-// }
+}
 
 
 
