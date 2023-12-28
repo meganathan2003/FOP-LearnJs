549de91a4b24501fc4660d169d6ff18cad093e0e
Meganathan <meganathan212003@gmail.com>
2023-12-28 20:58:10 +0530

    first commit in dev branch

--- JS LEARNING BASIC/Arrays.js
+++ JS LEARNING BASIC/Arrays.js
@@ -12,6 +12,8 @@ let colors = ["blue","black","white"] // It represent the arrays
 console.log(colors[1]);
 
 
+
+
 //  if we add a new colors into the colors we can
 colors[3] = "violet" // Also we can delete it and update it
 // console.log(colors.indexOf("white"));
